be7adde0f6f50c2bec6ccd067c6346b9d61717fa
Ganesh <ganeshgarje716@gmail.com>
2026-06-12 23:24:24 +0530

    Create StudentsRepository interface

---
+++ SpringBoot-8/src/main/java/com/ganesh/repository/StudentsRepository.java
@@ -0,0 +1,10 @@
+package com.ganesh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ganesh.entiry.Students;
+
+public interface StudentsRepository extends JpaRepository<Students, Integer>{
+	
+
+}
