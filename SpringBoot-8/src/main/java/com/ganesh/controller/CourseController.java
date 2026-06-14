950a785c396efb43b0fb82f0855e153f2aab24d1
Ganesh <ganeshgarje716@gmail.com>
2026-06-15 01:11:31 +0530

    Add methods

--- SpringBoot-8/src/main/java/com/ganesh/controller/CourseController.java
+++ SpringBoot-8/src/main/java/com/ganesh/controller/CourseController.java
@@ -1,11 +1,16 @@
 package com.ganesh.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ganesh.entiry.StudentDto;
 import com.ganesh.entiry.Students;
 import com.ganesh.service.StudentsService;
 
@@ -30,11 +35,20 @@ public class CourseController {
 	@PostMapping("/save")
 	public String saveStudent(Students student, Model model) {
 
-		service.saveStudent(student);
-        
-        model.addAttribute("msg", "Student Enrolled Sucessfully");
+		Students savedStudent = service.saveStudent(student);
+			
+	    return "redirect:/dashboard/"+savedStudent.getId();
+	}
+	
+	
+	@GetMapping("/dashboard/{id}")
+	public String getStudentDashboard(@PathVariable int id, Model model) {
 		
-	    return "redirect:/enroll";
+		StudentDto student = service.getById(id);
+		
+		model.addAttribute("student", student);
+
+		return "dashboard";
 	}
 	
 	
@@ -49,6 +63,17 @@ public class CourseController {
 	public String getCourses() {
 		
 		return "courses";
+	}
+	
+	
+	@GetMapping("/students")
+	public String getAllStudents(Model model) {
+		
+		List<StudentDto> Students = service.getAllStudents();
+		
+		model.addAttribute("students", Students);
+		
+		return "students";
 	}
 	
 	
