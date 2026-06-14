58fad906b7d233efab2e8ae685db21be2bfdc54c
Ganesh <ganeshgarje716@gmail.com>
2026-06-14 22:06:25 +0530

    Create methods

--- SpringBoot-8/src/main/java/com/ganesh/controller/CourseController.java
+++ SpringBoot-8/src/main/java/com/ganesh/controller/CourseController.java
@@ -1,15 +1,56 @@
 package com.ganesh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ganesh.entiry.Students;
+import com.ganesh.service.StudentsService;
 
 @Controller
 public class CourseController {
 	
+	@Autowired
+	StudentsService service;
+	
+	
 	@GetMapping("/enroll")
-	public String getEndrollmentForm() {
+	public String getEndrollmentForm(Model model) {
+		
+		Students student=new Students();
+				
+		model.addAttribute("student", student);
 		
 		return "endrollment-form";
 	}
+	
+	
+	@PostMapping("/save")
+	public String saveStudent(Students student, Model model) {
+
+		service.saveStudent(student);
+        
+        model.addAttribute("msg", "Student Enrolled Sucessfully");
+		
+	    return "redirect:/enroll";
+	}
+	
+	
+	@GetMapping("/")
+	public String homePage() {
+		
+		return "home";
+	}
+	
+	
+	@GetMapping("/courses")
+	public String getCourses() {
+		
+		return "courses";
+	}
+	
+	
 
 }
