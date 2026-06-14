f4ba8d28671116490771df663b435e9790696f6e
Ganesh <ganeshgarje716@gmail.com>
2026-06-15 01:10:32 +0530

    Create StudentDto Class

---
+++ SpringBoot-8/src/main/java/com/ganesh/entiry/StudentDto.java
@@ -0,0 +1,27 @@
+package com.ganesh.entiry;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+	
+	
+    private Integer id;
+	
+	private String name;
+	
+	private String course;
+	
+	private String gender;
+	
+	private String timings;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String address;
+	
+	private Double courseFees;
+
+}
