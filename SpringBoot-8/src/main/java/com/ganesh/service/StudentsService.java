066b5c96910c30c93f9cb9363cd4e6cff75e666a
Ganesh <ganeshgarje716@gmail.com>
2026-06-14 22:07:18 +0530

    Create methods

--- SpringBoot-8/src/main/java/com/ganesh/service/StudentsService.java
+++ SpringBoot-8/src/main/java/com/ganesh/service/StudentsService.java
@@ -1,10 +1,12 @@
 package com.ganesh.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ganesh.entiry.Students;
 import com.ganesh.repository.StudentsRepository;
 
+@Service
 public class StudentsService {
 	
 	
@@ -13,6 +15,19 @@ public class StudentsService {
 	
 	
 	public void saveStudent(Students student) {
+		
+		if (student.getCourse().equalsIgnoreCase("Java Full Stack")) {
+			
+			student.setCourseFees(50000.00);
+		}
+		else if (student.getCourse().equalsIgnoreCase("Python Full Stack")) {
+			
+			student.setCourseFees(40000.00);
+		}
+        else if (student.getCourse().equalsIgnoreCase("Data Science")) {
+			
+			student.setCourseFees(70000.00);
+		}
 		
 		repository.save(student);
 	}
